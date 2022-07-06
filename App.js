6db0649478291bae1922df723e115250ba5d6760
Torian Coleman <toriancoleman@Torians-MacBook-Pro.local>
2022-07-06 13:11:15 -0500

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.Pagecontainer}>
+        <View style={styles.card}>
+          <ImageBackground source={{
+            uri: 'https://media.istockphoto.com/photos/close-up-golf-ball-on-green-grass-picture-id1097225062?k=20&m=1097225062&s=612x612&w=0&h=dxHG5ZL84pWjgVKD2xlB0CzCMMAhHVdWdsMVLYMKaRU='}}
+              style={styles.img}>
+                <Text style={styles.name}> Torian </Text>
+                <Text style={styles.desc}> I'm the greatest alive </Text>
+            <StatusBar style="auto" />
+            
+          </ImageBackground>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  Pagecontainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    height: '100%',
+    width: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end'
+    
+   
+  },
+  card: {
+    height: '55%',
+    width: '75%',
+    backgroundColor: 'red',
+    borderRadius: 10,
+    marginBottom: 50, 
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+
+    elevation: 11,
+  },
+  name: {
+    fontSize: 40,
+    color: 'silver',
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginRight: 10,
+  },
+  desc: {
+    fontSize: 20,
+    color: 'white',
+    flexWrap: 'wrap',
+    marginBottom: 20,
+  }
 });
---
+++ assets/data/userdata.js
@@ -0,0 +1,4 @@
+const user = {
+    name: 'Torian',
+    img: 'https://media.istockphoto.com/photos/close-up-golf-ball-on-green-grass-picture-id1097225062?k=20&m=1097225062&s=612x612&w=0&h=dxHG5ZL84pWjgVKD2xlB0CzCMMAhHVdWdsMVLYMKaRU='
+}
