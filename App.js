6b8c64474a19345a4441461c04d05d06147e8f5a
Torian Coleman <toriancoleman@Torians-MacBook-Pro.local>
2022-07-07 20:25:59 -0500

    Swipe feature

--- App.js
+++ App.js
@@ -1,18 +1,81 @@
+import 'react-native-gesture-handler';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { StyleSheet, Pressable, Text, View, Image, ImageBackground } from 'react-native';
 import TinderCard from './src/components/TinderCard';
 import userdata from './assets/data/userdata';
+import Animated,{ 
+  useSharedValue, 
+  useAnimatedStyle, 
+  withSpring,
+  useAnimatedGestureHandler, 
+  useDerivedValue,
+  interpolate,}
+  from 'react-native-reanimated';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import useWindowDimensions from 'react-native/Libraries/Utilities/useWindowDimensions';
 
 
+const ROTATION = 60;
 
 
 export default function App() {
+const [currentIndex, setCurrentIndex] = useState(0);
+const [NexttIndex, setNextIndex] = useState(currentIndex + 1);
+
+
+const currentProfile = userdata[currentIndex];
+const nextProfile = userdata[NexttIndex]
+
+const {width: screenWidth} = useWindowDimensions();
+
+const hiddentranslateX = 2 * screenWidth;
+
+const tranlateX = useSharedValue(0);
+const rotate = useDerivedValue(
+  () => interpolate(tranlateX.value, [0, hiddentranslateX], [0, ROTATION]) + 'deg',
+  );
+
+
+const CMstyle = useAnimatedStyle(() => ({
+transform: [
+  {
+    translateX: tranlateX.value,
+  },
+  {
+    rotate: rotate.value,
+  }
+],
+}))
+
+const gestureHandler = useAnimatedGestureHandler({
+  onStart: (_, context) => {
+    context.startX = tranlateX.value
+  },
+  onActive: (event, context) => {
+    tranlateX.value = context.startX + event.translationX;
+  },
+  onEnd: () => {
+
+  },
+});
+
   return (
     <View style={styles.Pagecontainer}>
-      <TinderCard user={userdata[0]}/>
-      <StatusBar style="auto" />
+
+      <View style={styles.nextProfile}>
+          <TinderCard user={nextProfile}/>
+      </View>
+      
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+        <Animated.View style={[styles.animatedstyles, CMstyle]}>
+          <TinderCard user={currentProfile}/>
+          <StatusBar style="auto" />
+        </Animated.View>
+      </PanGestureHandler>
+
     </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
@@ -21,4 +84,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  animatedstyles: {
+    width: '100%',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nextProfile: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'center',
+  
+  }
 });
--- assets/data/userdata.js
+++ assets/data/userdata.js
@@ -1,6 +1,23 @@
 export default [{
     name: 'Torian',
-    des: 'Testing',
+    desc: 'Testing',
     image: 'https://media.istockphoto.com/photos/close-up-golf-ball-on-green-grass-picture-id1097225062?k=20&m=1097225062&s=612x612&w=0&h=dxHG5ZL84pWjgVKD2xlB0CzCMMAhHVdWdsMVLYMKaRU='
+},
+{
+    name: 'Lamar',
+    desc: 'Im that nigga',
+    image: 'https://www.losverdesgc.com/app/uploads/sites/24/2019/11/8fd6a464-court-prather-h7avq-7ffpw-unsplash.jpg'
+},
+{
+name: 'Catrell',
+desc: 'Kiss the ring',
+image: 'https://res.cloudinary.com/usga/image/upload/c_fill,dpr_3.0,f_auto,fl_lossy,h_429,q_auto,w_764/v1/usga/images/course-care/2022-photoshoot/22GH_87A2119'
+},
+{
+    name: 'Slight',
+    desc: 'Know what it is',
+    image: 'https://gsites.brightspotcdn.com/dims4/default/b43e03c/2147483647/strip/true/crop/7360x2786+0+1063/resize/1440x545!/brightness/-4x10/quality/90/?url=http%3A%2F%2Fgolf-brightspot.s3.amazonaws.com%2Fgsites%2F54%2F66%2Ffb665df84570920cb3306f390108%2Fgolfnat-15-back-high.jpg'
 }
+
+
 ]
